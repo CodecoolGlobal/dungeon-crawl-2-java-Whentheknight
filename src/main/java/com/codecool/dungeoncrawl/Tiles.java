9dd401bc97561360f76c0ed069d0dace97c7b084
Whentheknight <szepzeno@gmail.com>
2021-05-25 14:50:05 +0200

    Add different enemy types

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -28,6 +28,10 @@ public class Tiles {
         tileMap.put("floor", new Tile(2, 0));
         tileMap.put("player", new Tile(27, 0));
         tileMap.put("skeleton", new Tile(29, 6));
+        tileMap.put("closed door", new Tile(7,10));
+        tileMap.put("open door", new Tile(8, 10));
+        tileMap.put("ghost", new Tile(26,6));
+        tileMap.put("bat", new Tile(26,8));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
