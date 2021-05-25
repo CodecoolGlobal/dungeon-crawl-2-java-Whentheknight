07260c76634e0456c7679678377bb9e64483d6d0
Taowathar <taowathar@gmail.com>
2021-05-25 14:53:35 +0200

    Add basic items

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -3,12 +3,23 @@ package com.codecool.dungeoncrawl.logic;
 public enum CellType {
     EMPTY("empty"),
     FLOOR("floor"),
-    WALL("wall");
+    WALL("wall"),
+    SWORD("sword"),
+    KEY1("key1", 1),
+    KEY2("key2", 1),
+    HEALTH_POTION("health potion");
 
     private final String tileName;
+    private int stat = 0;
+
 
     CellType(String tileName) {
         this.tileName = tileName;
+    }
+
+    CellType(String tileName, int stat) {
+        this.tileName = tileName;
+        this.stat = stat;
     }
 
     public String getTileName() {
---
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
@@ -0,0 +1,16 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Key extends Item {
+    private final int keyNum;
+
+    public Key (Cell cell, int keyNum) {
+        super(cell);
+        this.keyNum = keyNum;
+    }
+
+    public String getTileName() {
+        return "key" + keyNum;
+    }
+}
