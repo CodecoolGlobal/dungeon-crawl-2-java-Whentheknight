91cd648c5cb5db54aff3f9a8874c4c68ee1f28c6
Whentheknight <szepzeno@gmail.com>
2021-05-27 15:42:54 +0200

    Add that player can't move to empty tile

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -83,7 +83,9 @@ public class Player extends Actor {
         }
 
         if (!nextCell.getType().equals(CellType.WALL) && currentEnemy == null &&
-            !nextCell.getType().equals(CellType.CDOOR) && !nextCell.getType().equals(CellType.TREE) && !nextCell.getType().equals(CellType.RIVER) && !nextCell.getType().equals(CellType.WALL2)) {
+            !nextCell.getType().equals(CellType.CDOOR) && !nextCell.getType().equals(CellType.TREE)
+                && !nextCell.getType().equals(CellType.RIVER) && !nextCell.getType().equals(CellType.WALL2)
+        && !nextCell.getType().equals(CellType.EMPTY)) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
