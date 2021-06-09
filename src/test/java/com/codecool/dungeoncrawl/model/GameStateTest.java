3308731aadfe2f1820f10691d0e513e13c4bab46
Whentheknight <szepzeno@gmail.com>
2021-06-09 11:26:24 +0200

    Checkout commit

---
+++ src/test/java/com/codecool/dungeoncrawl/model/GameStateTest.java
@@ -0,0 +1,13 @@
+package com.codecool.dungeoncrawl.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameStateTest {
+    @BeforeEach
+    void init(){
+    GameState gameState = new GameState();}
+
+    
+
+}
