fe69b31573d98059e0408b736aa012c013ec2959
Taowathar <taowathar@gmail.com>
2021-06-09 13:39:33 +0200

    Add KeyTest.java

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/KeyTest.java
@@ -0,0 +1,32 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.CellType;
+import com.codecool.dungeoncrawl.logic.GameMap;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class KeyTest {
+    GameMap gameMap;
+    Item key;
+
+    @BeforeEach
+    public void init() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        key = new Key(gameMap.getCell(1,2), 1);
+    }
+
+    @Test
+    @Order(1)
+    public void getTitleName_true() {
+        assertEquals("key", key.getTileName());
+    }
+
+    @Test
+    @Order(2)
+    public void toChar_true() {
+        assertEquals('k', key.toChar());
+    }
+
+}
