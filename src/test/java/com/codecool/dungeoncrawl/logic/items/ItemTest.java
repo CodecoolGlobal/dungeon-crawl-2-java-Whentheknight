5dce83dc32e020ff3064366ee9185c2a3e24b8bd
Taowathar <taowathar@gmail.com>
2021-06-09 14:12:40 +0200

    Add ItemTest.java

--- src/test/java/com/codecool/dungeoncrawl/logic/items/ItemTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/ItemTest.java
@@ -1,25 +1,24 @@
 package com.codecool.dungeoncrawl.logic.items;
 
-
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import com.codecool.dungeoncrawl.logic.actors.Player;
-import com.codecool.dungeoncrawl.logic.items.Axe;
-import com.codecool.dungeoncrawl.logic.items.Item;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ItemTest {
-    GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-    Player player = new Player(gameMap.getCell(1, 1));
+    GameMap gameMap;
+    Player player;
 
+    @BeforeEach
+    public void init() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        player = new Player(gameMap.getCell(1, 1));
+    }
 
     @Test
     @Order(1)
@@ -27,5 +26,85 @@ public class ItemTest {
         Item axe = new Axe(gameMap.getCell(1,2), "axe");
         Cell expected = gameMap.getCell(1,2);
         assertEquals(expected, axe.getCell());
+    }
+
+    @Test
+    @Order(2)
+    public void getX_true() {
+        Item axe = new Axe(gameMap.getCell(1,2), "axe");
+        assertEquals(1, axe.getCell().getX());
+    }
+
+    @Test
+    @Order(3)
+    public void getY_true() {
+        Item axe = new Axe(gameMap.getCell(1,2), "axe");
+        assertEquals(2, axe.getCell().getY());
+    }
+
+    @Test
+    @Order(4)
+    public void getStrength_true() {
+        Item axe = new Axe(gameMap.getCell(1,2), "axe");
+        assertEquals(10, axe.getStrength());
+    }
+
+    @Test
+    @Order(5)
+    public void setStrength() {
+        Item axe = new Axe(gameMap.getCell(1,2), "axe");
+        axe.setStrength(120);
+        assertEquals(120, axe.getStrength());
+    }
+
+    @Test
+    @Order(6)
+    public void getHealth_true() {
+        Item bearSteak = new BearSteak(gameMap.getCell(1,2));
+        assertEquals(5, bearSteak.getHealth());
+    }
+
+    @Test
+    @Order(7)
+    public void setHealth_true() {
+        Item bearSteak = new BearSteak(gameMap.getCell(1,2));
+        bearSteak.setHealth(100);
+        assertEquals(100, bearSteak.getHealth());
+    }
+
+    @Test
+    @Order(8)
+    public void getDodgeChance_true() {
+        Item leatherBoots = new LeatherBoots(gameMap.getCell(1,2));
+        assertEquals(0.1f, leatherBoots.getDodgeChance());
+    }
+
+    @Test
+    @Order(9)
+    public void setDodgeChance_true() {
+        Item leatherBoots = new LeatherBoots(gameMap.getCell(1,2));
+        leatherBoots.setDodgeChance(0.5f);
+        assertEquals(0.5f, leatherBoots.getDodgeChance());
+    }
+
+    @Test
+    @Order(10)
+    public void toChar_true() {
+        Item leatherBoots = new LeatherBoots(gameMap.getCell(1,2));
+        assertEquals('l', leatherBoots.toChar());
+    }
+
+    @Test
+    @Order(11)
+    public void hasStats_itemWithStats_true() {
+        Item axe = new Axe(gameMap.getCell(1,2), "axe");
+        assertTrue(axe.hasStats());
+    }
+
+    @Test
+    @Order(12)
+    public void hasStats_itemWithNoStats_false() {
+        Item key = new Key(gameMap.getCell(1,2), 1);
+        assertFalse(key.hasStats());
     }
 }
