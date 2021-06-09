efb25cc0df56ceaf557e830369995755e8320f2d
Taowathar <taowathar@gmail.com>
2021-06-09 13:24:01 +0200

    Add BearSteakTest.java

--- src/test/java/com/codecool/dungeoncrawl/logic/items/BearSteakTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/BearSteakTest.java
@@ -1,44 +1,47 @@
 package com.codecool.dungeoncrawl.logic.items;
 
+
+
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class BatSoupTest {
+public class BearSteakTest {
     GameMap gameMap;
-    Item batSoup;
+    Item bearSteak;
 
     @BeforeEach
     public void init() {
         gameMap = new GameMap(3, 3, CellType.FLOOR);
-        batSoup = new BatSoup(gameMap.getCell(1,2));
+        bearSteak = new BearSteak(gameMap.getCell(1,2));
     }
 
     @Test
     @Order(1)
     public void getTitleName_true() {
-        assertEquals("bat soup", batSoup.getTileName());
+        assertEquals("bear steak", bearSteak.getTileName());
     }
 
     @Test
     @Order(2)
     public void getStrength_true() {
-        assertEquals(-3, batSoup.getStrength());
+        assertEquals(2, bearSteak.getStrength());
     }
 
     @Test
     @Order(3)
     public void getHealth_true() {
-        assertEquals(-3, batSoup.getHealth());
+        assertEquals(5, bearSteak.getHealth());
     }
 
     @Test
     @Order(4)
     public void toChar_true() {
-        assertEquals('u', batSoup.toChar());
+        assertEquals('E', bearSteak.toChar());
     }
+
 }
