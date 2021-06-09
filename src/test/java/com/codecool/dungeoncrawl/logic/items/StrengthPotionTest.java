76760e00de91f25f63444bdbb053fe6f373f9d74
Taowathar <taowathar@gmail.com>
2021-06-09 13:36:57 +0200

    Add StrengthPotionTest.java

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/StrengthPotionTest.java
@@ -0,0 +1,38 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import com.codecool.dungeoncrawl.logic.CellType;
+import com.codecool.dungeoncrawl.logic.GameMap;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StrengthPotionTest {
+    GameMap gameMap;
+    Item strengthPotion;
+
+    @BeforeEach
+    public void init() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        strengthPotion = new StrengthPotion(gameMap.getCell(1,2));
+    }
+
+    @Test
+    @Order(1)
+    public void getTitleName_true() {
+        assertEquals("strength potion", strengthPotion.getTileName());
+    }
+
+    @Test
+    @Order(2)
+    public void getStrength_true() {
+        assertEquals(10, strengthPotion.getStrength());
+    }
+
+    @Test
+    @Order(3)
+    public void toChar_true() {
+        assertEquals('P', strengthPotion.toChar());
+    }
+
+}
